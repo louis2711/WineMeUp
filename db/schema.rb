fb53472b9a7dd0e3a83185577b17c0acc5aad432
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-05-26 12:24:42 +0200

    Created questions and answers seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_092233) do
+ActiveRecord::Schema.define(version: 2020_05_26_101701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_092233) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "question_type"
   end
 
   create_table "users", force: :cascade do |t|
