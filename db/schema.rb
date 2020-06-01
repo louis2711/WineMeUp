e54bbf94a439b2315bcfe35757482ce48c641ccf
Louis Fresnel <louisfresnel@live.fr>
2020-06-01 10:21:36 +0200

    description seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_091502) do
+ActiveRecord::Schema.define(version: 2020_06_01_081715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_05_29_091502) do
     t.string "aroma"
     t.string "country"
     t.string "label"
+    t.text "description"
   end
 
   create_table "feedbacks", force: :cascade do |t|
