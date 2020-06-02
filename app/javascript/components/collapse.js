7ed35946b5f6c68c5b242d2fad65f06bfdfc70d4
Stanislas de Skowronski <stanislas.deskowronski@gmail.com>
2020-06-02 16:56:58 +0200

    - to + JS

--- app/javascript/components/collapse.js
+++ app/javascript/components/collapse.js
@@ -1,27 +1,18 @@
-const collapseY = () => {
+const collapse = () => {
   const crosses = document.querySelectorAll('.container-filters a');
   if (crosses) {
     crosses.forEach((e)=>{
       e.addEventListener('click', (event) => {
-        let x = event.target.innerHTML.trim();
-        event.target.innerHTML = "-"
+        if (event.currentTarget.classList.contains('open')) {
+          event.currentTarget.innerHTML = "+";
+          event.currentTarget.classList.toggle('open');
+        } else {
+          event.currentTarget.innerHTML = "-";
+          event.currentTarget.classList.toggle('open');
+        }
       })
     })
   };
 };
 
-export { collapseY };
-
-const collapseX = () => {
-  const minuses = document.querySelectorAll('.container-filters a');
-  if (minuses) {
-    minuses.forEach((a)=>{
-      a.addEventListener('click', (minus) => {
-        let y = minus.target.innerHTML.trim();
-        minus.target.innerHTML = "+"
-      })
-    })
-  };
-};
-
-export { collapseX };
+export { collapse };
